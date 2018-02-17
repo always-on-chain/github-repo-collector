e4eb8aa49d84564c7408fc70778fd887ae8e7de2
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-02-17 12:59:45 -0800

    got repo info to render on page

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -3,6 +3,20 @@ import React from 'react';
 var RepoList = (props) => (
   <div>
     <h4> Repo List Component </h4>
+    <table>
+      <tr><td>ID</td><td>Name</td><td>User</td><td>Created</td><td>Forks</td><td>Watchers</td><td>Language</td></tr>
+      {props.repos.map(function(repo) {
+        return <tr>
+        <td>{repo.id}</td> 
+        <td>{repo.name}</td> 
+        <td>{repo.user}</td>
+        <td>{repo.created}</td>
+        <td>{repo.forks}</td> 
+        <td>{repo.watchers}</td>
+        <td>{repo.language}</td>
+        </tr>
+      })}
+    </table>
     There are {props.repos.length} repos.
   </div>
 )
